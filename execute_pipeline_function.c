a8a3a3f4c06a56437c06ecb9864bc132023c6960
Hari <149360252+Harimino123@users.noreply.github.com>
2024-12-02 16:32:46 +0300

    Create execute_pipeline_function.c

---
+++ execute_pipeline_function.c
@@ -0,0 +1,69 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+
+/*ONLY DRAFT, NOT THE REAL ONE FOR NOW*/
+
+void execute_pipeline(char **args, t_env *envp)
+{
+    int i = 0;
+    int pipefd[2];
+    pid_t pid;
+    int prev_fd = 0;  // Tracks the previous pipe's read end
+
+    // Iterate through the commands separated by pipes
+    while (args[i] != NULL) {
+        // Set up a pipe only if this isn't the last command in the pipeline
+        if (args[i + 1] != NULL) {
+            if (pipe(pipefd) == -1) {
+                perror("pipe");
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        pid = fork();
+        if (pid == -1) {
+            perror("fork");
+            exit(EXIT_FAILURE);
+        }
+
+        if (pid == 0) {  // Child process
+            // Redirect the input from the previous command's output
+            if (prev_fd != 0) {
+                dup2(prev_fd, STDIN_FILENO);
+                close(prev_fd);
+            }
+
+            // If this is not the last command, redirect output to the pipe
+            if (args[i + 1] != NULL) {
+                dup2(pipefd[1], STDOUT_FILENO);
+                close(pipefd[0]);
+                close(pipefd[1]);
+            }
+
+            // Execute the current command
+            if (execvp(args[i], args) == -1) {
+                perror("execvp");
+                exit(EXIT_FAILURE);
+            }
+        } else {  // Parent process
+            // Close the write end of the pipe (in parent)
+            if (args[i + 1] != NULL) {
+                close(pipefd[1]);
+            }
+
+            // Save the read end of the current pipe for the next iteration
+            prev_fd = pipefd[0];
+
+            // Wait for the child process to finish (we'll wait for the last one later)
+            waitpid(pid, NULL, 0);
+        }
+
+        // Move to the next command in the pipeline
+        i++;
+    }
+
+    // Wait for all child processes to finish
+    while (wait(NULL) > 0);
+}
